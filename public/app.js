d6b3d18abd7b39e4c7da96c9eabd4e594f7b3ded
Jamison Cozart <jamisoncozart@gmail.com>
2019-12-18 07:25:25 -0800

    Add new todos through input form post requests

--- public/app.js
+++ public/app.js
@@ -2,14 +2,36 @@ $(document).ready(function(){
     $.getJSON("/api/todos")
     .then(function(data){
         data.forEach(function(todo){
-            let newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>')
-            if(todo.completed){
-                newTodo.addClass("done");
-            }
-            $(".list").append(newTodo);
+            addTodo(todo);
         })
     })
     .catch(function(err){
         console.log(err);
     });
+    $("#todoInput").keypress(function(event){
+        if(event.which == 13) {
+            //send request to create new todo
+            var userInput = $("#todoInput").val();
+            $.post('/api/todos', {name: userInput})
+            .then(function(newTodo){
+                addTodo(newTodo);
+                $("#todoInput").val("");
+            })
+            .catch(function(err){
+                console.log(err);
+            })
+        }
+    });
+    // $(".task").click(function(task){
+    //     $.put('/api/todos', {name: })
+    //     task.addClass("done");
+    // })
 })
+
+function addTodo(todo) {
+    let newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>')
+            if(todo.completed){
+                newTodo.addClass("done");
+            }
+            $(".list").append(newTodo);
+}
